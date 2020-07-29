7eab0dd67ebd0aea8e76e6d6a9577e8766bf69d6
tatarintsev <tatalexey@gmail.com>
2020-07-30 00:59:48 +0300

    Выолнил все задания 1-го занятия

--- resources/views/greeting.php
+++ resources/views/greeting.php
@@ -7,19 +7,28 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <style>
-        a: {
+        a {
             text-decoration: none;
+            transition: all .2s;
+            color: black;
         }
-        a:hover {
 
+        a:hover {
+            color: gray;
+        }
+
+        .menu {
+            list-style-type: none;
+            width: 50vh;
+            display: flex;
+            justify-content: space-around;
         }
     </style>
 </head>
 <body>
-    <h1>Приветствуем учебном проекте Laravel от GeekBrains</h1>
-    <ul class="menu">
-        <li><a href="/info/">Информация</a></li>
-        <li><a href="/news/">Новости</a></li>
-    </ul>
+<h1>Приветствуем учебном проекте Laravel от GeekBrains</h1>
+<ul class="menu">
+    <li><a href="/info/">Информация</a></li>
+    <li><a href="/news/">Новости</a></li>
 </body>
 </html>
--- resources/views/info.php
+++ resources/views/info.php
@@ -1,2 +1,25 @@
-<h1>TEst</h1>
-<?php echo phpinfo()?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        a {
+            text-decoration: none;
+            transition: all .2s;
+            color: black;
+        }
+        a:hover {
+            color: gray;
+        }
+    </style>
+</head>
+<body>
+    <h1>Информация о проекте</h1>
+    <p>Это будет агрегатор новостей</p>
+    <p><a href="/?>">Назад</a></p>
+</body>
+</html>
